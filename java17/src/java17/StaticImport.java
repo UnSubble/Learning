57ec5b013f86c1e3c46f5ad48aecf5d8a2f7e6cb
İsmail Kulak <unsubble@gmail.com>
2024-02-18 18:03:02 +0300

    static import

---
+++ java17/src/java17/StaticImport.java
@@ -0,0 +1,16 @@
+package java17;
+
+// import static pckg2.StaticImportSample // DOES NOT COMPILE
+// static import pckg2.StaticImportSample // DOES NOT COMPILE
+import static pckg2.StaticImportSample.variable; // eğer farklı bir paketteki aynı isimli değişken veya 
+import static pckg2.StaticImportSample.method; // metotu da import etseydik. Compile sıkıntısı çıkardı.
+
+public class StaticImport {
+	
+	public static void main(String[] args) {
+		System.out.println(variable);
+		method();
+		// StaticImportSample.method(); // DOES NOT COMPILE
+	}
+	
+}
---
+++ java17/src/pckg2/StaticImportSample.java
@@ -0,0 +1,9 @@
+package pckg2;
+
+public class StaticImportSample {
+	public static int variable = 5;
+	
+	public static void method() {
+		System.out.println("static method");
+	}
+}
