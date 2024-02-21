4a1aeddc015395583ab560d603a379c92c35d1ce
İsmail Kulak <unsubble@gmail.com>
2024-02-21 19:51:23 +0300

    LambdasAndFunctionalInterfaces updated.

--- java17/src/java17/LambdasAndFunctionalInterfaces.java
+++ java17/src/java17/LambdasAndFunctionalInterfaces.java
@@ -1,5 +1,10 @@
 package java17;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
 interface MyInterface {
 	boolean perform(String str);
 }
@@ -23,6 +28,35 @@ public class LambdasAndFunctionalInterfaces {
 		
 		// var invalid = (String x) -> x.startsWith("a"); // DOES NOT COMPILE çünkü var ne 
 															// döneceğini algılayamıyor.
+		
+		Consumer<Integer> consumer = (var x) -> System.out.println(x); // var kullanabiliriz.
+		
+		List<Integer> list = new ArrayList<>(); 
+		list.sort((final var x, @Deprecated var y) -> x.compareTo(y)); // final @Deprecated kullanabiliriz.
+													// Desteklenen annotationların hepsini kullanabiliriz.
+		
+		// BiFunction<Integer, String, Integer> func = 
+		//		(Integer x, y) -> Integer.parseInt(y) + x; // DOES NOT COMPILE bir tanesi tanımlanmışsa 
+															// diğerinin de tanımlanması gerekir.
+		
+		// BiFunction<Integer, String, Integer> func =
+		//		(Integer x, var y) -> Integer.parseInt(y) + x; // DOES NOT COMPILE çünkü bir tip belirtilip
+															// diğeri var olamaz.
+		
+		BiFunction<Integer, Integer, Integer> func =
+				(x, y) -> {int z = 0; return x + y + z; };
+		
+		
+	}
+	
+	private String color;
+	private static int size = 5;
+	
+	public void caw(String name) {
+		String volume = "loudly";
+		Consumer<String> consumer = s -> System.out.println(name + " says " + volume + size +
+				" that she is " + color); // Eğer değişkenler effectively final veya final ise lambdada 
+		// name = "a"; // eğer yorumdan kaldırılırsa üstteki lambda compile hatası verir.																				// kullanılabilir.
 	}
 }
 
