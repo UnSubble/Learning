bbc0707cda1d0626cfe5507f718044a8f4ccd090
İsmail Kulak <unsubble@gmail.com>
2024-02-25 01:48:04 +0300

    ConsoleCommands added.

---
+++ java17/src/java17/ConsoleCommands.java
@@ -0,0 +1,13 @@
+package java17;
+
+public class ConsoleCommands {
+	// javac -d <Derleneceği yol> <.java dosyasının olduğu yol>
+	// javac -cp <import ettiğimiz ayrı class'ların bulunduğu yol> <.java dosyasının olduğu yol>
+		// örnek: javac -cp src src/com/unsubble/Main.java
+		// bu örnekte src altında bulunan bütün yollara bakarak ilgili .java dosyalarını bulur ve compile 
+																									 // eder.
+	// -cp yerine --class-path veya -classpath de kullanabiliriz.
+	// jar -cvf <File>.jar <Path>
+		// buradaki c -> create v -> verbose f -> file
+	
+}
