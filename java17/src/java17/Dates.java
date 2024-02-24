6248aabce939d6d30c490cb5d0309aa52bf8dd86
İsmail Kulak <unsubble@gmail.com>
2024-02-24 21:23:36 +0300

    Dates updated.

--- java17/src/java17/Dates.java
+++ java17/src/java17/Dates.java
@@ -2,12 +2,18 @@ package java17;
 
 import java.text.DateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class Dates {
@@ -51,6 +57,28 @@ public class Dates {
 		System.out.println(DateFormat.getDateInstance(DateFormat.LONG).format(date));
 		System.out.println(DateFormat.getDateInstance(DateFormat.FULL).format(date));
 		
+		LocalDateTime dt = LocalDateTime.of(2022, Month.APRIL, 20, 15, 12, 35);
+		Locale us = new Locale("en","US");
+		DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+		System.out.println(dtf.withLocale(us).format(dt)); // 4/20/22
+		System.out.println(dtf.format(dt)); // 20.04.2022
+		dtf = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+		System.out.println(dtf.withLocale(us).format(dt)); // 3:12 PM
+		System.out.println(dtf.format(dt)); // 15:12
+		dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.SHORT);
+		System.out.println(dtf.withLocale(us).format(dt)); // 4/20/22, 3:12 PM
+		System.out.println(dtf.format(dt)); // 20.04.2022 15:12
 		
+		ZonedDateTime zdt = ZonedDateTime.of(dt, ZoneId.of("Europe/Istanbul"));
+		
+		dtf = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+		System.out.println(dtf.withLocale(us).format(zdt)); // April 20, 2022
+		System.out.println(dtf.format(zdt)); // 20 Nisan 2022
+		dtf = DateTimeFormatter.ofLocalizedTime(FormatStyle.LONG);
+		System.out.println(dtf.withLocale(us).format(zdt)); // 3:12:35 PM TRT
+		System.out.println(dtf.format(zdt)); // 15:12:35 TRT
+		dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.LONG);
+		System.out.println(dtf.withLocale(us).format(zdt)); // April 20, 2022 at 3:12:35 PM TRT
+		System.out.println(dtf.format(zdt)); // 20 Nisan 2022 15:12:35 TRT
 	}
 }
