4c99a9d6f3a041799812cb89c267b2b173537987
İsmail Kulak <unsubble@gmail.com>
2024-02-24 21:30:22 +0300

    Localization updated.

--- java17/src/java17/Localization.java
+++ java17/src/java17/Localization.java
@@ -1,5 +1,9 @@
 package java17;
 
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Locale;
 
 public class Localization {
@@ -14,5 +18,32 @@ public class Localization {
 		System.out.println(l1.getDisplayLanguage()); // İngilizce
 		System.out.println(l1.getDisplayLanguage(Locale.FRANCE)); // anglais
 		System.out.println(l1.getDisplayLanguage(l1)); // English
+		
+		var localeTr = new Locale("tr", "TR");
+		var money = 1.23;
+		var today = LocalDate.now();
+		
+		Locale.setDefault(new Locale("en", "US"));
+		
+		System.out.println(NumberFormat.getCurrencyInstance().format(money)); // $1.23
+		System.out.println(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).format(today));
+																					// February 24, 2024
+		System.out.println(localeTr.getDisplayLanguage()); // Turkish
+		
+		Locale.setDefault(Locale.Category.DISPLAY, localeTr);
+		
+		System.out.println(NumberFormat.getCurrencyInstance().format(money)); // $1.23
+		System.out.println(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).format(today));
+																					// February 24, 2024
+		System.out.println(localeTr.getDisplayLanguage()); // Türkçe
+		
+		Locale.setDefault(Locale.Category.FORMAT, localeTr);
+		
+		System.out.println(NumberFormat.getCurrencyInstance().format(money)); // ₺1,23
+		System.out.println(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).format(today)); 
+																					// 24 Şubat 2024
+		System.out.println(localeTr.getDisplayLanguage()); // Türkçe
+		
+		
 	}
 }
