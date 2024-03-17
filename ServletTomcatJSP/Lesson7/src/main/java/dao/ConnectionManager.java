fca7348169aa218304b7ba7bdeddd1e25d0ca7cf
İsmail Kulak <unsubble@gmail.com>
2024-03-17 21:49:38 +0300

    updated.

--- ServletTomcatJSP/Lesson7/src/main/java/dao/ConnectionManager.java
+++ ServletTomcatJSP/Lesson7/src/main/java/dao/ConnectionManager.java
@@ -1,11 +1,4 @@
 package dao;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.ResourceBundle;
-
 import javax.sql.DataSource;
 
 import org.sqlite.SQLiteDataSource;
