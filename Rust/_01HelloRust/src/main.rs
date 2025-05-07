d4a8edff567c04bd4f70709f939361d502f7eddf
Unsubble <unsubble@gmail.com>
2025-05-07 16:20:58 +0300

    Rust Lesson #2

--- Rust/_01HelloRust/src/main.rs
+++ Rust/_01HelloRust/src/main.rs
@@ -1,4 +1,33 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: i8,
+    work: String
+}
 
+struct Point(i32, i32);
+
+impl Person {
+    fn new(name: &str, age: i8, work: &str) -> Person /* Self de yazabilirdik */ {
+        Person {
+            name: name.to_string(),
+            age,
+            work: work.to_string()
+        }
+    }
+
+    // self, &self, mut self, &mut self
+    fn get_age(&self) -> i8 {
+        self.age
+    }
+}
+
+#[derive(Debug)]
+enum Shape {
+    Circle(f64),
+    Rectangle(f64, f64),
+    Triangle(f64, f64, f64)
+}
 
 fn main() {
     let mut x: u32 = 10;
@@ -40,11 +69,35 @@ fn main() {
 
     println!("{result}");
 
-    println!("{}", return_param(x as i64));
+    println!("{}", return_param(x as i64)) ;
     println!("{}", return_multiple().1);
 
     let (r, s) = return_multiple();
     println!("{} {}", r, s);
+
+    let p1 = Person {
+        name: "a".to_string(),
+        age: 26,
+        work: "dentist".to_string()
+    };
+
+    println!("{p1:?}");
+
+    let p2 = Person {
+        name: "b".to_string(),
+        ..p1
+    };
+
+    println!("{:?}", p2);
+
+    println!("{}", p2.get_age());
+
+    let point = Point(5, 6);
+    println!("{}, {}", point.0, point.1);
+
+    let shape = Shape::Rectangle(18_f64, 28_f64);
+
+    println!("{:?}", shape);
 }
 
 fn return_param(x: i64) -> i64 {
