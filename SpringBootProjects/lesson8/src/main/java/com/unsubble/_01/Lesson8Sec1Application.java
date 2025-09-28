f6ef898e53e96e7df95940a21718ac09fb797aea
unsubble <unsubble@gmail.com>
2025-09-28 22:40:03 +0300

    Spring Boot Lesson 8 updated.

--- SpringBootProjects/lesson8/src/main/java/com/unsubble/_01/Lesson8Sec1Application.java
+++ SpringBootProjects/lesson8/src/main/java/com/unsubble/_01/Lesson8Sec1Application.java
@@ -1,10 +1,13 @@
 package com.unsubble._01;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @SpringBootApplication(scanBasePackages = "com.unsubble")
 @Controller
 public class Lesson8Sec1Application {
+
+    @InitBinder // data'mız bin edilirken bu kurallar uygulanır ilk önce
+    public void initTrimBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        binder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping(value = "/getFormPage", method = RequestMethod.GET)
     public String getFormPage(Model model) {
@@ -24,7 +34,7 @@ public class Lesson8Sec1Application {
                            BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors())
             return "formPage";
-        System.out.println(person.getName());
+        System.out.println(person.getName() + " " + person.getAge());
         model.addAttribute("person", person);
         return "validPage";
     }
--- SpringBootProjects/lesson8/src/main/java/com/unsubble/_01/Person.java
+++ SpringBootProjects/lesson8/src/main/java/com/unsubble/_01/Person.java
@@ -1,6 +1,6 @@
 package com.unsubble._01;
 
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Person {
+    @NotNull(message = "is required!")
     @Size(min = 4, max = 16, message = "is required!")
     private String name;
+
+    @NotNull(message = "age cannot be empty")
+    @Min(value = 18, message = "age cannot be less than 18")
+    @Max(value = 100, message = "age cannot be greater than 100")
+    private Integer age;
+
+    @Pattern(regexp = "[\\w_.]+@\\w+\\.\\w+", message = "invalid email address")
+    private String email;
 }
